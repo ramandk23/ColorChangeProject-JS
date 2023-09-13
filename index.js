aca160d27e9a4f08c3fd6b1d7a46da64a948da18
ramandk23 <ramandk23@gmail.com>
2023-09-13 11:13:15 +0800

    1 file modified

--- index.js
+++ index.js
@@ -12,14 +12,8 @@ buttons.forEach(function (btn) {
     console.log(e.target);
     switch (e.target.id) {
       case 'grey':
-        body.style.backgroundColor = e.target.id;
-        break;
       case 'blue':
-        body.style.backgroundColor = e.target.id;
-        break;
       case 'yellow':
-        body.style.backgroundColor = e.target.id;
-        break;
       case 'white':
         body.style.backgroundColor = e.target.id;
         break;
